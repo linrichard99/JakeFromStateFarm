d05ee1123ddfb519c9c540949abc5123fbdca43c
Vincent Liok <vincentliok@Vincents-MacBook-Pro.local>
2016-01-16 19:52:59 -0500

    Finalized PDA.java

--- PDA.java
+++ PDA.java
@@ -3,34 +3,17 @@ import java.text.*;
 import java.io.*;
 
 
-public class PDA implements Printer{
+public class PDA extends Printer{
 
     private InputStreamReader inVal;
     private BufferedReader readVal;
-    private boolean goToMain;
+    private boolean goToMain; //flag that allows the PDA to constantly run
 
     public static void main (String[] args) {
+
 	PDA IEH = new PDA();
-	IEH.printMain();
 	
-	while (IEH.goToMain) {
-	    
-	    String input = "";
-
-	    try {
-		input = IEH.readVal.readLine();
-	    }
-
-	    catch ( IOException e) {}
-
-
-	    if (input.equals("Time")) {
-		System.out.println();
-		System.out.println("Time:" + IEH.getTime());
-		IEH.printMain();
-	    }
-	       
-	}		
+	IEH.run();
 	
     }
 
@@ -40,31 +23,21 @@ public class PDA implements Printer{
 	goToMain = true;
     }
 
+    //get date and time
+
     public String getDateSimple() {
 	Date currentDate = new Date(); //Instantiating the date
 	SimpleDateFormat displayDate = new SimpleDateFormat("E yyyy-MM-dd"); //Instantiating the SimpleDateFormat object
 	return "Date: " + displayDate.format(currentDate); //this line displays the date the way we want it to
     }
 
-    //Printer interface implementations
-
-    //%-s makes it the following string left justified
-    //The 15 means width of 15
-    //The \t and \n are just tabs and new lines spectively
-    //Non-static because the interface does not allow for static values
-    public void print(String arg) {
-	System.out.printf("|\t %-35s|", arg); 
+    public String getTime() {
+        Date currentDate = new Date(); //Instantiating the date                                                                                                                                                                                                               
+        SimpleDateFormat displayTime = new SimpleDateFormat("hh:mm:ss");
+        return displayTime.format(currentDate);
     }
 
-    public void println(String arg) {
-	System.out.printf("|\t %-35s|\n", arg);
-    }
-
-    public void println() {
-	System.out.printf("|\t %-35s|\n", "");
-    }
-    
-    //Printing the screen
+    //two different screens can be displayed
 
     public void printMain() {
 	printLine();
@@ -84,15 +57,50 @@ public class PDA implements Printer{
 	printLine();
     }
 
-    public void printLine() {
-	System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    public void printMainTime() {
+        printLine();
+        println();
+        println();
+        println(getDateSimple());
+        println(getTime());
+        println("<Time>");
+        println();
+        println("<Calendar>");
+        println("<Scheduler>");
+        println("<Contacts>");
+        println("<Memo>");
+        println("<Calculator>");
+        println();
+        println();
+        printLine();
+    }
+
+    public void run() {
+	printMain();
+	
+	while (goToMain) {
+	    
+	    String input = "";
+	    
+	    try {
+		input = readVal.readLine();
+	    }
+	    
+	    catch ( IOException e) {}
+	    
+	    if (input.equals("")) {
+		printMain();
+	    }
+
+	    if (input.equals("Exit")) {
+		goToMain = false;
+	    }
+	    
+	    if (input.equals("Time")) {
+		printMainTime();
+	    }
+	    
+	}
     }
     
-    public String getTime() {
-	Date currentDate = new Date(); //Instantiating the date
-	SimpleDateFormat displayTime = new SimpleDateFormat("hh:mm:ss");
-	return displayTime.format(currentDate);
-    }
-	
-
 }
--- Printer.java
+++ Printer.java
@@ -1,9 +1,23 @@
-public interface Printer {
+public abstract class Printer {
+
+    //%-s makes the following string left justified           
+    //The 15 means width of 15                               
+    //The \t and \n are just tabs and new lines spectively  
     
-    public void print(String arg);
+    public void print(String arg) {
+        System.out.printf("|\t %-35s|", arg);
+    }
 
-    public void println(String arg);
+    public void println(String arg) {
+        System.out.printf("|\t %-35s|\n", arg);
+    }
 
-    public void println();
+    public void println() {
+        System.out.printf("|\t %-35s|\n", "");
+    }
+
+    public void printLine() {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
 
 }
